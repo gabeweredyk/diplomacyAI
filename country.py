d5f8b1a03d73016d2480db2934edfa0c599ee010
arcsecondofparallax <lukedaignault@hotmail.com>
2023-11-16 16:26:34 -0500

    this isn't anything important

--- country.py
+++ country.py
@@ -1,9 +1,3 @@
-class Country(object):
-    def __init__(self, n, a, f):
-        self.name = n
-        self.armies = a
-        self.fleets = f
-
 class Army(object):
     def __init__(self, loc):
         self.location = loc
@@ -12,3 +6,8 @@ class Fleet(object):
     def __init__(self, loc):
         self.location = loc
     
+class Country(object):
+    def __init__(self, n, a, f):
+        self.name = n
+        self.armies = a
+        self.fleets = f
