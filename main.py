c762155fafb829dca7a9e189fa431ed95299f576
arcsecondofparallax <lukedaignault@hotmail.com>
2023-11-30 15:18:33 -0500

    hink

--- main.py
+++ main.py
@@ -14,5 +14,4 @@ async def gameloop(socket, created):
 
 
 if __name__ == "__main__":
-    server = input("Server IP: ").strip()
-    protocol = input("Join game or create game? (j/c): ").strip()
+    
--- messageAnalysis.py
+++ messageAnalysis.py
@@ -3,7 +3,36 @@ from country import *
 from analyzeMoves import *
 
 def messageAnalysis(prelimMoves,players,assignedCountry,territories,paths,trustDict):
+    expectations = []
     for player in players:
         pass
+    
     while True:
-        pass
+        incomingMessage = input("Incoming Message: ").strip()
+        incomingMessage = incomingMessage.split()
+        match incomingMessage[0]:
+            case "You":
+                match incomingMessage[1]:
+                    case "might":
+                        pass
+                    case "should":
+                        pass
+                    case "must":
+                        pass
+            case "We":
+                match incomingMessage[1]:
+                    case "might":
+                        pass
+                    case "should":
+                        pass
+                    case "must":
+                        pass
+            case "I":
+                match incomingMessage[1]:
+                    case "might":
+                        pass
+                    case "should":
+                        pass
+                    case "must":
+                        pass
+    return expectations
